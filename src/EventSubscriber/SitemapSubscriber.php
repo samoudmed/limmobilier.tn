90bac7003890a5ab5f09abde3c9098232a079f0d
SAMOUD Mohamed <samoud.mohamed@gmail.com>
2025-09-12 22:45:05 +0200

    SitemapSubscriber

---
+++ src/EventSubscriber/SitemapSubscriber.php
@@ -0,0 +1,67 @@
+<?php
+
+
+namespace App\EventListener;
+
+use App\Repository\BlogPostRepository;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Presta\SitemapBundle\Event\SitemapPopulateEvent;
+use Presta\SitemapBundle\Service\UrlContainerInterface;
+use Presta\SitemapBundle\Sitemap\Url\UrlConcrete;
+
+class SitemapSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var BlogPostRepository
+     */
+    private $blogPostRepository;
+
+    /**
+     * @param BlogPostRepository $blogPostRepository
+     */
+    public function __construct(BlogPostRepository $blogPostRepository)
+    {
+        $this->blogPostRepository = $blogPostRepository;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            SitemapPopulateEvent::class => 'populate',
+        ];
+    }
+
+    /**
+     * @param SitemapPopulateEvent $event
+     */
+    public function populate(SitemapPopulateEvent $event): void
+    {
+        $this->registerBlogPostsUrls($event->getUrlContainer(), $event->getUrlGenerator());
+    }
+
+    /**
+     * @param UrlContainerInterface $urls
+     * @param UrlGeneratorInterface $router
+     */
+    public function registerBlogPostsUrls(UrlContainerInterface $urls, UrlGeneratorInterface $router): void
+    {
+        $posts = $this->blogPostRepository->findAll();
+
+        foreach ($posts as $post) {
+            $urls->addUrl(
+                new UrlConcrete(
+                    $router->generate(
+                        'blog_post',
+                        ['slug' => $post->getSlug()],
+                        UrlGeneratorInterface::ABSOLUTE_URL
+                    )
+                ),
+                'blog'
+            );
+        }
+    }
+}
