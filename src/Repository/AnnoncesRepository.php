ebdbaa06a31b70142bf8bea5ec01b7a634cf55f8
SAMOUD Mohamed <samoud.mohamed@gmail.com>
2025-09-12 23:08:00 +0200

    AnnoncesRepository

--- src/Repository/AnnoncesRepository.php
+++ src/Repository/AnnoncesRepository.php
@@ -9,7 +9,7 @@ use App\Entity\Types;
 use App\Entity\User;
 use Doctrine\Persistence\ManagerRegistry;
 
-class AnnoncesRepository extends EntityRepository {
+class AnnoncesRepository extends ServiceEntityRepository {
 
     public function __construct(ManagerRegistry $registry)
     {
