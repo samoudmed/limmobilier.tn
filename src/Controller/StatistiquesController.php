1313e96cc42f1f58c870e3cec1d144074ba3165e
samoudmed <40462553+samoudmed@users.noreply.github.com>
2025-09-25 08:17:51 +0000

    Statistiques page

--- src/Controller/StatistiquesController.php
+++ src/Controller/StatistiquesController.php
@@ -1,3 +1,61 @@
+    /**
+     * @Route("/estimation-bien-tunisie.html", name="estimation_bien_tunisie", methods={"GET", "POST"})
+     */
+    public function estimationBienTunisie(Request $request)
+    {
+        $gouvernorats = array_keys($this->getPrixReference());
+        $types = [];
+        foreach ($this->getPrixReference() as $gouv => $typeArr) {
+            foreach (array_keys($typeArr) as $type) {
+                if (!in_array($type, $types)) {
+                    $types[] = $type;
+                }
+            }
+        }
+
+        $resultat = null;
+        if ($request->isMethod('POST')) {
+            $gouv = $request->request->get('gouvernorat');
+            $type = $request->request->get('type');
+            $surfaceTotale = (float)$request->request->get('surface_totale');
+            $surfaceBat = (float)$request->request->get('surface_batie');
+            $chambres = (int)$request->request->get('chambres');
+            $options = $request->request->get('options', []);
+
+            $prixRef = $this->getPrixReference();
+            $prixM2 = isset($prixRef[$gouv][$type]) ? $prixRef[$gouv][$type] : 0;
+            $prixBase = $surfaceTotale * $prixM2;
+
+            // Bonus options (exemple simple)
+            $bonus = 0;
+            if (is_array($options)) {
+                foreach ($options as $opt) {
+                    if ($opt === 'jardin') $bonus += 0.05 * $prixBase;
+                    if ($opt === 'garage') $bonus += 0.03 * $prixBase;
+                    if ($opt === 'piscine') $bonus += 0.08 * $prixBase;
+                    if ($opt === 'terrasse') $bonus += 0.02 * $prixBase;
+                }
+            }
+            $prixEstime = $prixBase + $bonus;
+
+            $resultat = [
+                'prix_estime' => round($prixEstime, 0),
+                'prix_m2' => $prixM2,
+                'gouvernorat' => $gouv,
+                'type' => $type,
+                'surface_totale' => $surfaceTotale,
+                'surface_batie' => $surfaceBat,
+                'chambres' => $chambres,
+                'options' => $options
+            ];
+        }
+
+        return $this->render('default/estimation_bien_tunisie.html.twig', [
+            'gouvernorats' => $gouvernorats,
+            'types' => $types,
+            'resultat' => $resultat
+        ]);
+    }
 <?php
 
 namespace App\Controller;
